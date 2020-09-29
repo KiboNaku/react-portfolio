a3d0c7ba604c47008b54d10fc09112433af057a0
Andy Ni <nakukibo@gmail.com>
2020-09-29 11:31:54 -0500

    Updates

---
+++ precache-manifest.841a5d8cc5c41d86af53cb48c3f6b434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76faae25ddd1ffadfc36e4965c46a739",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1cc7cea0cc69401e0cd7",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "27b4d124137cbcd4c603",
+    "url": "/react-portfolio/static/css/main.b4fe1218.chunk.css"
+  },
+  {
+    "revision": "1cc7cea0cc69401e0cd7",
+    "url": "/react-portfolio/static/js/2.3ba3d831.chunk.js"
+  },
+  {
+    "revision": "48708d0bba57d355d4ffc14a8bfac5cf",
+    "url": "/react-portfolio/static/js/2.3ba3d831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b4d124137cbcd4c603",
+    "url": "/react-portfolio/static/js/main.8014a378.chunk.js"
+  },
+  {
+    "revision": "07badd340c7a9ea09cef",
+    "url": "/react-portfolio/static/js/runtime-main.d6f65571.js"
+  }
+]);
